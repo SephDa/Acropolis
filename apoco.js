b4f85fe974bdd77f1c2ac949d35b204052973a6d
Samantha <samantha.j.h@live.com>
2020-05-20 15:03:48 +0100

    Added transform to intro menu

--- apoco.js
+++ apoco.js
@@ -4,8 +4,17 @@ document.getElementById("eg button").addEventListener('click', function() {
     } 
 );
 */
-
+/*
 window.onload = function menu(){
-    document.getElementById("menuToggle").style.display = 'none';
-    document.getElementById("menu").style.display = 'block';
-}
+    document.getElementById("menuBox").style.display = 'block';
+}*/
+
+document.getElementById('brandNewGame').addEventListener('click', function () {
+    document.getElementById('menuBox').style.display = 'none';
+    document.getElementById('menuToggle').style.display = 'block';
+}) 
+
+document.getElementById('brandNewGame').addEventListener('click', function () {
+    document.getElementById('menuBox').style.display = 'none';
+    document.getElementById('menuToggle').style.display = 'block';
+}) 
