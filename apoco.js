d6f1344882141ecd9b94b68a8314cdf20c0abcf7
Samantha <samantha.j.h@live.com>
2020-05-20 16:55:26 +0100

    Added farm area and back Menu option

--- apoco.js
+++ apoco.js
@@ -14,7 +14,22 @@ document.getElementById('brandNewGame').addEventListener('click', function () {
     document.getElementById('menuToggle').style.display = 'block';
 }) 
 
-document.getElementById('brandNewGame').addEventListener('click', function () {
-    document.getElementById('menuBox').style.display = 'none';
-    document.getElementById('menuToggle').style.display = 'block';
+document.getElementById('backToMenuButton').addEventListener('click', function () {
+    document.getElementById('menuBox').style.display = 'block';
+    document.getElementById('menuToggle').style.display = 'none';
 }) 
+
+document.getElementById('saveButton').addEventListener('click', function () {
+    window.alert("Your game has been saved! Hooray!");
+}) 
+
+/*
+var farmPlot = document.querySelectorAll('.farmPlot');
+
+function farmClick() {
+    for (var i = 0; i < farmPlot.length; i++){
+        window.alert("Nothing has been planted here, yet! Better get cracking!");}
+}
+
+farmPlot.addEventListener('click',farmClick());
+*/
