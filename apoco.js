d47da8ca2d83da65cc4b0e17764b76d162004846
Samantha <samantha.j.h@live.com>
2020-05-26 17:14:21 +0100

    Added New game stats and Name JS code

--- apoco.js
+++ apoco.js
@@ -29,11 +29,11 @@ function onFarmClick() {
 };
 
 function getInput() {
-   
+    
     // Get the number of tiles value from the user
     var inputElement = document.getElementById("inputOfPlots");
     const amntTiles = inputElement.value;
-       
+        
     //Generate the number of divs based on the value above
     for (var i = 0; i < amntTiles; i++) {
         var farmPlot = document.createElement('div');
@@ -50,17 +50,44 @@ function getInput() {
     }
 }
 
-function generateTiles(amntTiles) {
+// Add user typed in name to welcome message
+function addName() {
+    var userName = document.getElementById("inputName");
+    var name = userName.value;
     
+    var welcomeNote = document.getElementById("welcome");
+    welcomeNote.textContent = "Welcome to Acropolis, " + name + "!"; 
 }
-
 
 function main() {
     
     document.getElementById('brandNewGame').addEventListener('click', getInput);
-     
+    document.getElementById('brandNewGame').addEventListener('click', addName); 
 }
 
 
 // Run the main functions of the page
 main();
+
+
+// Change input no of plots font
+/*var numeralInput = document.getElementById('inputOfPlots');
+
+numeralInput.addEventListener('click', function() {
+    if (numeralInput.textContent === "") {
+        numeralInput.style.fontFamily = "MyWebFont";
+        numeralInput.placeholder = "Enter no. of Plots";
+    } else {
+        numeralInput.classList.add('numerals');
+        numeralInput.placeholder = '';
+    };*/
+
+//Click Menu to clear all data and start again
+
+document.getElementById('backToMenuButton').addEventListener('click',clearAll);
+
+
+    function clearAll() {
+        document.getElementById("inputName").value = "";
+        document.getElementById("inputOfPlots").value = "";
+    }
