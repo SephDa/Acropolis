db64f46ce7a62106d478334eb29f2d87d17e6b04
Samantha <samantha.j.h@live.com>
2020-05-27 17:37:47 +0100

    Added Clear all and Error checks for input

--- apoco.js
+++ apoco.js
@@ -1,23 +1,6 @@
-/*
-document.getElementById("eg button").addEventListener('click', function() {
-    document.getElementById("menu").style.display = 'none'
-    } 
-);
-*/
-/*
-window.onload = function menu(){
-    document.getElementById("menuBox").style.display = 'block';
-}*/
 
-document.getElementById('brandNewGame').addEventListener('click', function () {
-    document.getElementById('menuBox').style.display = 'none';
-    document.getElementById('menuToggle').style.display = 'block';
-}) 
 
-document.getElementById('backToMenuButton').addEventListener('click', function () {
-    document.getElementById('menuBox').style.display = 'block';
-    document.getElementById('menuToggle').style.display = 'none';
-}) 
+
 
 document.getElementById('saveButton').addEventListener('click', function () {
     window.alert("Your game has been saved! Hooray!");
@@ -28,26 +11,60 @@ function onFarmClick() {
     window.alert("Nothing has been planted here, yet! Better get cracking!");
 };
 
-function getInput() {
-    
+function startGame() {
     // Get the number of tiles value from the user
-    var inputElement = document.getElementById("inputOfPlots");
-    const amntTiles = inputElement.value;
-        
-    //Generate the number of divs based on the value above
-    for (var i = 0; i < amntTiles; i++) {
-        var farmPlot = document.createElement('div');
-        var parent = document.getElementById('farms');
+    var plotInputElement = document.getElementById("inputOfPlots");
+    amntTiles = plotInputElement.value;    
 
-        // Add Child div to parent Farm div
-        parent.appendChild(farmPlot);
+    //Get the username
+    var userName = document.getElementById("inputName");
+    var name = userName.value;
 
-        // Add class to newly created divs
-        farmPlot.classList.add('farmPlot');
-
-        // Run OnClick farmplot function
-        farmPlot.addEventListener('click', onFarmClick);
+    //Check if user has entered a name
+    if (name ===""){
+        window.alert("You must enter a user name!");
+        return;
     }
+
+    // Get crop type
+    var cropType = document.getElementById('fruitType').value;
+    
+    // Check if user has selected crop type
+    if (cropType === "initial") {
+        window.alert("You need to select a crop type");
+        return;
+    }
+
+    //Check if user has entered no. of plots above 0
+    if (amntTiles === "") {
+        window.alert("You need to enter a plot value!");
+        return;
+    } else if (amntTiles <= 0) {
+        window.alert("You must enter a plot value more than 0!");
+        return;
+    } else {
+        //Toggle Welcome Menu
+        toggleMenu(); 
+        addName();
+
+        //Generate the number of divs based on the value above
+        for (var i = 0; i < amntTiles; i++) {
+            var farmPlot = document.createElement('div');
+            var parent = document.getElementById('farms');
+
+            // Add Child div to parent Farm div
+            parent.appendChild(farmPlot);
+
+            // Add class to newly created divs
+            farmPlot.classList.add('farmPlot');
+
+            // Run OnClick farmplot function
+            farmPlot.addEventListener('click', onFarmClick);
+        }
+    
+
+
+    }   
 }
 
 // Add user typed in name to welcome message
@@ -59,35 +76,62 @@ function addName() {
     welcomeNote.textContent = "Welcome to Acropolis, " + name + "!"; 
 }
 
-function main() {
+//Clear the Gamedata 
+function clearAll() {
+    //Clear Name
+    document.getElementById("inputName").value = "";
+
+    //Clear No of Plots entered
+    document.getElementById("inputOfPlots").value = "";
+    //amntTiles = 0;
+
+    //Clear Crop Type
+    document.getElementById("fruitType").value = "initial";
+
+    //Delete the divs
     
-    document.getElementById('brandNewGame').addEventListener('click', getInput);
-    document.getElementById('brandNewGame').addEventListener('click', addName); 
+    var parent = document.getElementById('farms');
+    //var list = document.querySelectorAll('.farmPlot');
+
+    // Remove Child farmplot divs from parent Farm div
+    //parent.querySelectorAll('*').forEach(n => n.remove());
+
+    //This was my try: my own logic
+
+    /*for (i = 0; i < list.length; i++) {
+        if (i.class === "farmPlot") {
+            parent.removeChild(list[i])
+        }
+    }*/
+
+    //Try 2 // found online, makes sense
+    var child = parent.lastElementChild;
+    while (child){
+        parent.removeChild(child);
+        child = parent.lastElementChild;
+    }
+
+}
+
+function toggleMenu() {
+    document.getElementById('menuBox').style.display = 'none';
+    document.getElementById('menuToggle').style.display = 'block';
+}
+
+function toggleBackMenu() {
+    document.getElementById('menuBox').style.display = 'block';
+    document.getElementById('menuToggle').style.display = 'none';
 }
 
 
+//Run all the main functions
+function main() {
+    
+    document.getElementById('brandNewGame').addEventListener('click', startGame);
+    //Click Back to Main Menu to clear all data and start again
+    document.getElementById('backToMenuButton').addEventListener('click',clearAll);
+    document.getElementById('backToMenuButton').addEventListener('click', toggleBackMenu); 
+}
+
 // Run the main functions of the page
 main();
-
-
-// Change input no of plots font
-/*var numeralInput = document.getElementById('inputOfPlots');
-
-numeralInput.addEventListener('click', function() {
-    if (numeralInput.textContent === "") {
-        numeralInput.style.fontFamily = "MyWebFont";
-        numeralInput.placeholder = "Enter no. of Plots";
-    } else {
-        numeralInput.classList.add('numerals');
-        numeralInput.placeholder = '';
-    };*/
-
-//Click Menu to clear all data and start again
-
-document.getElementById('backToMenuButton').addEventListener('click',clearAll);
-
-
-    function clearAll() {
-        document.getElementById("inputName").value = "";
-        document.getElementById("inputOfPlots").value = "";
-    }
